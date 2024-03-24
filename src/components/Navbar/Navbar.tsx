c7ffc2dcc304a8681e699ee22484ddfe8e94d2af
Emmanuel Ochoa <eorivera95@gmail.com>
2024-03-23 23:28:45 -0600

    feat: rendering a button to add employees

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 import { ModeToggle } from '../ModeToggle/ModeToggle'
+import { Button } from '../ui/button'
+import { PersonIcon } from '@radix-ui/react-icons'
 
 const Navbar = () => {
   return (
     <nav className="py-4 flex justify-between items-center">
       <p className="text-lg font-medium">FictiCorp</p>
-      <ModeToggle />
+      <div className="flex justify-center items-center gap-4">
+        <Button>
+          <PersonIcon height={14} className="mr-4" />
+          Nuevo Empleado
+        </Button>
+        <ModeToggle />
+      </div>
     </nav>
   )
 }
